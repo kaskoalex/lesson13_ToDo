1296bdc5df5dde957495c9b88c3eaafdaffecf75
Alex <webmaster@papagai.de>
2022-02-15 19:00:12 +0100

    create

--- js/script.js
+++ js/script.js
@@ -3,41 +3,78 @@ const todoControl = document.querySelector('.todo-control') // form class="todo-
 const headerInput = document.querySelector('.header-input') // input class="header-input" type = "text" placeholder = "Какие планы?" 
 const todoList = document.querySelector('.todo-list') //<ul class="todo todo-list" id="todo">
 const todoCompleted = document.querySelector('.todo-completed')  // <ul class="todo todo-completed" id="completed">
+const todoContainer = document.querySelector('.todo-container')  // <div class="todo-container">
 
-const toDoData = [
-  {
-    text:'Сварить кофе',
-    completed: false
-  },
-  {
-    text: 'Помыть посуду',
-    completed: true
-  }
-]
+const toDoData = []   // база занесённых задачь
 
 const render = function () {
-  toDoData.forEach(function(item){
+  todoList.innerHTML = ''    // очищаем от предыдущих запесей
+  todoCompleted.innerHTML = ''
+  toDoData.forEach(function (item) {
 
-    console.log(item)
+    const li = document.createElement('li') // создаём елемент 'li'
+    li.classList.add('todo-item') // добовляем в 'li' todo-item
+    li.innerHTML = '<span class="text-todo">' + item.text + '</span>' +
+      '<div class="todo-buttons">' +
+      '<button class="todo-remove"></button>' +
+      '<button class="todo-complete"></button>' +
+      '</div> '    // добавляем в li текст переберая item  со сссылкой на техт
+
+
+    // проверка на true и false
+    if (item.completed) {
+      todoCompleted.append(li)  // вставляем данные елементы в Completed если true
+
+    } else {
+      todoList.append(li)  // вставляем данные елементы в todoList
+
+    }
+    // вешаем на кнопку обработчик событий
+    li.querySelector('.todo-complete').addEventListener('click', function() {
+      item.completed = !item.completed
+      render()
+    }); 
+    //Удаление дел на кнопку КОРЗИНА
+    li.querySelector('.todo-remove').addEventListener('click', function () {
+      const index = toDoData.indexOf(item);
+      toDoData.splice(index, 1);
+      
+      render();
+    });
 
   })   // функция по перебору событий, тудушек
 }
 
 
-todoControl.addEventListener('submit', function(event)  // фукция выполнения события
+todoControl.addEventListener('submit', function (event)  // фукция выполнения события
 {
   event.preventDefault()
 
+  // проверка на введение текста
+  while (headerInput.value==='') {
+    headerInput.style.backgroundColor = 'hotpink';
+    alert('Введите текст');    
+    return
+  }
+
   const newToDo =    // создали новый объект newToDo
-    {
-      text: headerInput.value,  // текст берётся из inputa
-      completed: false
-    }
+  {
+    text: headerInput.value,  // текст берётся из inputa
+    completed: false
+  }
 
-     toDoData.push(newToDo)
-     headerInput.value = ''  // очищение строчки от надписи
+  toDoData.push(newToDo)
+  headerInput.value = ''  // очищение строчки от надписи
 
-     render()
+
+  render()
 
 })
 
+//Удаление дел на кнопку КОРЗИНА
+
+console.log(todoContainer)
+
+console.dir(todoContainer)
+console.dir(todoContainer.children[0])
+console.log(todoContainer.children[0].children.HTMLCollection)
